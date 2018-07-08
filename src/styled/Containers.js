356fd3edd778ac2ffe1b1a64924a8c2c8bf25bff
Kitefr <kitefr@gmail.com>
2018-07-08 17:42:21 +0200

    Add FlexContainer

--- src/styled/Containers.js
+++ src/styled/Containers.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 import { StyledH1 } from "./Headers";
+import { StyledCard } from "./Card";
 
 export const StyledAppContainer = styled.div`
   width: 100vw;
@@ -23,5 +24,23 @@ export const StyledPageContainer = styled.div`
   ${StyledH1} {
     line-height: 3rem;
     border-bottom: 1px solid black;
+  }
+`;
+
+export const FlexContainer = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+
+  ${StyledCard} {
+    width: 100%;
+  }
+
+  @media screen and (min-width: 768px) {
+    flex-flow: row wrap;
+    justify-content: space-between;
+
+    ${StyledCard} {
+      width: 48%;
+    }
   }
 `;
