84ae6f7a7ab8cab3e7723269460da21fe12c316c
Kitefr <kitefr@gmail.com>
2018-07-08 16:06:47 +0200

    Add basic router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import { StyledAppContainer } from "./styled/Containers";
+
+import { Route } from "react-router-dom";
+import Loader from "./components/Loader";
+import Home from "./components/Home";
+import Character from "./components/Character";
 
 class App extends Component {
   render() {
-    return <div />;
+    return (
+      <StyledAppContainer>
+        <Route exact path="/" component={Home} />
+        <Route path="/characters/:id" component={Character} />
+      </StyledAppContainer>
+    );
   }
 }
 
